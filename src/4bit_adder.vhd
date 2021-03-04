462b51f566c33c196fbd1f1df97f10e47182e8be
Jerzy Stefanowicz <jerzy.stefanowicz@iceye.com>
2021-03-04 21:07:48 +0100

    rozwiazania

--- src/4bit_adder.vhd
+++ src/4bit_adder.vhd
@@ -1,3 +1,7 @@
+-------------------------------------------------------------------------------
+-- File Downloaded from http://www.nandland.com
+-------------------------------------------------------------------------------
+
 entity adder4bit is
   port (
     i_in1     : in bit_vector(3 downto 0);
@@ -11,9 +15,54 @@ end adder4bit;
 
 architecture rtl of adder4bit is
 
-
+  component full_adder is
+  port (
+    i_bit1  : in bit;
+    i_bit2  : in bit;
+    i_carry : in bit;
+    --
+    o_sum   : out bit;
+    o_carry : out bit
+  );
+  end component;
+  signal carry_sig : bit_vector(2 downto 0);
 begin
   
+  ad0 : full_adder
+  port map(
+    i_bit1  => i_in1(0),
+    i_bit2  => i_in2(0),
+    i_carry => '0',
+    o_sum   => o_sum(0),
+    o_carry => carry_sig(0)
+  );
+
+  ad1 : full_adder
+  port map(
+    i_bit1  => i_in1(1),
+    i_bit2  => i_in2(1),
+    i_carry => carry_sig(0),
+    o_sum   => o_sum(1),
+    o_carry => carry_sig(1)
+  );
+
+  ad2 : full_adder
+  port map(
+    i_bit1  => i_in1(2),
+    i_bit2  => i_in2(2),
+    i_carry => carry_sig(1),
+    o_sum   => o_sum(2),
+    o_carry => carry_sig(2)
+  );
+
+  ad3 : full_adder
+  port map(
+    i_bit1  => i_in1(3),
+    i_bit2  => i_in2(3),
+    i_carry => carry_sig(2),
+    o_sum   => o_sum(3),
+    o_carry => o_carry
+  );
 
   
 end rtl;
--- src/full_adder.vhd
+++ src/full_adder.vhd
@@ -1,3 +1,7 @@
+-------------------------------------------------------------------------------
+-- File Downloaded from http://www.nandland.com
+-------------------------------------------------------------------------------
+
 entity full_adder is
   port (
     i_bit1  : in bit;
@@ -12,6 +16,20 @@ end full_adder;
 
 architecture rtl of full_adder is
 
+  signal w_WIRE_1 : bit;
+  signal w_WIRE_2 : bit;
+  signal w_WIRE_3 : bit;
+
 begin
 
+  w_WIRE_1 <= i_bit1 xor i_bit2;
+  w_WIRE_2 <= w_WIRE_1 and i_carry;
+  w_WIRE_3 <= i_bit1 and i_bit2;
+  
+  o_sum   <= w_WIRE_1 xor i_carry;
+  o_carry <= w_WIRE_2 or w_WIRE_3;
+  -- FYI: Code above using wires will produce the same results as:
+  -- o_sum   <= i_bit1 xor i_bit2 xor i_carry;
+  -- o_carry <= (i_bit1 xor i_bit2) and i_carry) or (i_bit1 and i_bit2);
+  -- Wires are just used to be explicit. 
 end rtl;
